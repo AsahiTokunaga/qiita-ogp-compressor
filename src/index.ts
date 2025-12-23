346cbf0427e8be620547502d3bf6e1991cd1fd53
AsahiTokunaga <hihimamug14@gmail.com>
2025-12-23 13:25:40 +0900

    feat: Qiita OGP image bypassing api endpoint

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,74 @@
  */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request): Promise<Response> {
+		const url = new URL(request.url);
+		const res = await fetch(`https://qiita.com${url.pathname}`, { redirect: 'follow' });
+		switch (res.status) {
+			case 400:
+				return new Response('Bad Request', { status: res.status });
+			case 401:
+				return new Response('Unauthorized', { status: res.status });
+			case 403:
+				return new Response('Forbidden', { status: res.status });
+			case 404:
+				return new Response('Not Found', { status: res.status });
+			default:
+				if (!res.ok) return new Response(null, { status: res.status });
+		}
+
+		let ogType: string | null = null;
+		let ogTitle: string | null = null;
+		let ogDescription: string | null = null;
+		let themeColor: string | null = null;
+		await new HTMLRewriter()
+			.on('meta[property="og:type"]', {
+				element(element) {
+					ogType = element.getAttribute('content');
+				}
+			})
+			.on('meta[property="og:title"]', {
+				element(element) {
+					ogTitle = element.getAttribute('content');
+				}
+			})
+			.on('meta[property="og:description"]', {
+				element(element) {
+					ogDescription = element.getAttribute('content');
+				}
+			})
+			.on('meta[name="theme-color"]', {
+				element(element) {
+					themeColor = element.getAttribute('content');
+				}
+			})
+			.transform(res)
+			.text();
+		if (!ogType || !ogTitle || !ogDescription || !themeColor) return new Response('Not Found', { status: 404 });
+
+		const response = `
+			<!DOCTYPE html>
+			<head>
+				<meta name="theme-color" content="${themeColor}">
+				<meta name="twitter:card" content="summary_large_image">
+				<meta property="og:site_name" content="Qiita">
+				<meta property="og:type" content="${ogType}">
+				<meta property="og:title" content="${ogTitle}">
+				<meta property="og:description" content="${ogDescription}">
+				<meta property="og:url" content="https://qiita.com${url.pathname}">
+				<meta property="og:image" content="https://qiita-ogpimage-getter.hihimamu.workers.dev${url.pathname}">
+			</head>
+			<body>
+				<p>Redirecting...</p>
+				<script>
+					window.location.href = "https://qiita.com${url.pathname}";
+				</script>
+			</body>
+			</html>
+		`;
+		return new Response(response, {
+			headers: { 'Content-Type': 'text/html; charset=utf-8' },
+		});
 	},
 } satisfies ExportedHandler<Env>;
+
